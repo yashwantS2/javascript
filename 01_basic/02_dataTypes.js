e8961d90074bdf76f6e54e3bdfe0c3b41eb8d671
yashwants7 <yashwants72509@gmail.com>
2023-10-27 22:58:57 +0530

    datatype complete

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,27 @@
+console.log(3+3)
+
+//alert(3+3) we are using node js not browser
+
+
+let age = 18 
+//number 
+//big int for big numbers larger nos 
+
+let name = "Yashwant"
+
+//string 
+
+let isIndian = true
+
+//boolean
+
+let temperature = null
+
+//null value having no value assigned to it
+
+//symbol -> unique
+
+console.log(typeof isIndian)
+
+//type of null is object
+//type of undefined is undefined
