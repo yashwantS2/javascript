4f1edde62926700f078e3e2b4be8e9a5192b8bf9
yashwants7 <yashwants72509@gmail.com>
2023-10-27 23:30:42 +0530

    postfix prefix

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let age = "20"
+
+console.log(age)
+
+let convertedAge = Number(age)
+
+console.log(convertedAge)
+console.log(typeof(convertedAge))
+console.log(typeof(age))
+
+//converted "20" -> 20
+//"45ab" -> Nan (Not a number)
+// true -> 1 ; false -> 0
+//"" -> false
+let isTrue = 0
+
+let convertBoolean = Boolean(isTrue)
+console.log(convertBoolean)
+
+/********OPERATION**************/
+
+let counter = 99
+
+let ind= counter++
+//prefix and post fir
+console.log(ind)
+console.log(counter)
+
+//reference of the postfix and prefix :  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+//documentation for study
