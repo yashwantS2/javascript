d3b9aece0a761715f13c0dfbb9c85dc3a4f7a578
yashwants7 <yashwants72509@gmail.com>
2023-10-28 00:05:55 +0530

    comparison with null and undefined

---
+++ 01_basic/04_comparison.js
@@ -0,0 +1,13 @@
+console.log(2>1)
+console.log(2==1)
+console.log(2==2)
+console.log(3!=5)
+
+
+console.log("2" > 1)
+
+console.log( undefined == 0 )
+
+//=== triple equal checks datatype too
+
+console.log("2"===2)//false because it check data type of the the operators
