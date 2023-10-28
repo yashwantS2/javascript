5e4699bb92a6580d342a0a3d21b12c151f54e9bf
yashwants7 <yashwants72509@gmail.com>
2023-10-28 14:15:54 +0530

    heaps with object

---
+++ 06_stackHeap.js
@@ -0,0 +1,16 @@
+//In stack always get copy of the object or variable
+//but in heap you will get reference to that object like in 
+
+let employee ={
+    name:"Yashant",
+    age: 20,
+    address: "India",
+    email:"yash@gmail.com"
+}
+console.log(employee.email)
+let employee2 = employee
+employee2.email="india@gmail.com"
+
+
+console.log(employee2.email)
+console.log(employee.email)
