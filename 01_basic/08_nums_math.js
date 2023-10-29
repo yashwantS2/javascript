aa538250edb935efbe82e5a8cf5f1084bbdf2c94
yashwants7 <yashwants72509@gmail.com>
2023-10-29 15:38:49 +0530

    Math.random()

--- 01_basic/08_nums_math.js
+++ 01_basic/08_nums_math.js
@@ -17,10 +17,11 @@ const money = 100000000
 console.log(money.toLocaleString('en-IN'));
 
 
-//(((((((((((((((((MATHS)))))))))))))))))
+//    (((((((((((((((((MATHS)))))))))))))))))
 
 
 console.log(Math);
+
 console.log(Math.floor(4.66));
 console.log(Math.round(4.66));
 console.log(Math.ceil(4.66));
@@ -29,9 +30,13 @@ console.log(Math.pow(2,3));
 console.log(Math.min(2,3,4,5,6,10,7));
 
 
+
 console.log(Math.random()); //0-1 eg 0.002,0.112 etc. 
 console.log((Math.random()*10)+1);
 console.log(Math.floor((Math.random()*10)+1));
 const min =1
 const max =6
+
 console.log(Math.floor((Math.random()*(max -min +1 ))+min));
+
+console.log(Math.floor(Math.random()* (max - min +1)+min))
