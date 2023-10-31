ddce99f694ee1387fce5806482c4da1fbd0c89ab
yashwants7 <yashwants72509@gmail.com>
2023-10-31 23:56:02 +0530

    assign in object

--- 02_basic/04_object2.js
+++ 02_basic/04_object2.js
@@ -10,3 +10,12 @@ obj1.email ="yudhvir123@email.com"
 obj1.age =18
 
 console.log(obj1)
+
+const obj3 ={a: 1,b:2 ,c:3}
+const obj4 ={d: 4,e:5 ,f:6}
+
+const merge = Object.assign({},obj3,obj4)
+console.log(merge);
+
+const obj5 ={...obj3,...obj4}
+console.log(obj5);
