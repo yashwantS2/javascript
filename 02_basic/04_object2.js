0abd0225c6ab50d313e9018b39424e066fad432e
yashwants7 <yashwants72509@gmail.com>
2023-10-31 23:50:26 +0530

    singelton obj

---
+++ 02_basic/04_object2.js
@@ -0,0 +1,12 @@
+//Both are object
+
+const obj1 = new Object()//singolten object
+console.log(obj1);
+const obj2 = {} //non-singleton obbject
+console.log(obj2);
+
+obj1.name ="Yudhvir"
+obj1.email ="yudhvir123@email.com"
+obj1.age =18
+
+console.log(obj1)
