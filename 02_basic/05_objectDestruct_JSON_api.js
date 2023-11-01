2e33336dd3861224295269374b42a7d735da7316
yashwants7 <yashwants72509@gmail.com>
2023-11-01 21:17:39 +0530

    JSON

---
+++ 02_basic/05_objectDestruct_JSON_api.js
@@ -0,0 +1,33 @@
+//object de structure
+
+const course ={
+    courseID : 2234,
+    courseName : "DSA",
+    price : 999,
+    courseInstructor : "Yashwant Singh",
+
+}
+//de structuring
+const {courseID : Id } = course
+
+console.log(Id)
+
+//json
+
+//basically is is type of object but difference is 
+//both key and value should be in string except bool and nos.
+
+//JSON
+// {
+//     "name" : "India",
+//     "WinnerIn" : "Asian Games",
+//     "WinID" : 987,
+//     "isINDIAN" : true
+// }
+
+[
+    {},
+    {},
+    {},
+    {}
+]
