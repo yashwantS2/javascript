d1d36903d3b8f8c0511a4f27f9d2ac0aa8980e99
yashwants7 <yashwants72509@gmail.com>
2023-11-02 23:33:54 +0530

    rtn parameter in console

--- 03_basic/01_function.js
+++ 03_basic/01_function.js
@@ -40,4 +40,33 @@ function calculateSum(...num1){
 
 console.log(calculateSum(200, 900 , 700 ))
 
+//object and fuction
+function calculateSum2(val1, val2,...num1){
+    return num1
+}
 
+console.log(calculateSum2(200, 900 , 700 , 88, 9090 ,990 ))
+
+const user ={
+    username : "Indian0098",
+    ID : 556
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and ID is : ${anyobject.ID}`)
+}
+
+handleObject(user)
+
+handleObject({
+    username : "hijj998",
+    ID: 878
+})
+
+const myNewArray = [200, 500 , 800 ,900]
+
+function rtnOneIdx(getArray){
+    return getArray[1]
+}
+
+console.log(rtnOneIdx([200,3000,789,899,990,877,899]))
